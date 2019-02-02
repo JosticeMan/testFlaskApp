c959e478fd68ab20b1867081d242637b337579f5
Justin Yau <justiceman8998@gmail.com>
2019-02-02 16:56:36 -0500

    Added database implementation in cojunction with login/register

--- Flask/flask_app/__init__.py
+++ Flask/flask_app/__init__.py
@@ -4,15 +4,22 @@ Version: 1.1
 Description: First flask app for educational and testing purposes
 Author: Justin Yau
 Source: Tutorial followed can be found at https://www.youtube.com/watch?v=QnDWIZuWYW0
-Packages: Flask, Flask-SQlAlchemy
+Packages: Flask, Flask-SQlAlchemy, Flask-BCrypt, Flask-Login
 """
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__, template_folder="templates")
 app.config['SECRET_KEY'] = 'cc567a24de5382dff2a86adcc98c3589'
 # 3 forward slashes tell the program to create the database within relative directory
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from flask_app import routes
--- Flask/flask_app/models.py
+++ Flask/flask_app/models.py
@@ -7,10 +7,16 @@ Source: Tutorial followed can be found at https://www.youtube.com/watch?v=QnDWIZ
 Packages: Flask, Flask-SQlAlchemy
 """
 from datetime import datetime
-from flask_app import db
+from flask_app import db, login_manager
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
