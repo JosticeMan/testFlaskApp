491a962d3a2103dc953d9a6b18103377c218ce94
Justin Yau <justiceman8998@gmail.com>
2019-02-02 15:39:14 -0500

    Added login/registration w/o database

--- Flask/flask_app.py
+++ Flask/flask_app.py
@@ -6,8 +6,12 @@ Author: Justin Yau
 Source: Tutorial followed can be found at https://www.youtube.com/watch?v=QnDWIZuWYW0
 Packages: Flask
 """
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__, template_folder="templates")
+
+app.config['SECRET_KEY'] = 'cc567a24de5382dff2a86adcc98c3589'
 
 posts = [
     {
@@ -32,9 +36,30 @@ def home():
     return render_template("home.html", posts=posts)
 
 
-@app.route("/about-me")
+@app.route("/about")
 def about():
-    return "Justin Yau RIT CS Student"
+    return render_template("about.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
---
+++ Flask/forms.py
@@ -0,0 +1,31 @@
+"""
+File: flask_app.py
+Version: 1.1
+Description: First flask app for educational and testing purposes
+Author: Justin Yau
+Source: Tutorial followed can be found at https://www.youtube.com/watch?v=QnDWIZuWYW0
+Packages: flask-wtf
+"""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
