2756be1f25ebf2729b4d81cd9b76ee95b9d68be3
Justin Yau <justiceman8998@gmail.com>
2019-02-02 14:22:23 -0500

    Added HTML file that can read information sent

--- Flask/flask_app.py
+++ Flask/flask_app.py
@@ -1,18 +1,33 @@
 """
 File: flask_app.py
-Version: 1.0
+Version: 1.1
 Description: First flask app for educational and testing purposes
 Author: Justin Yau
 """
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder="templates")
+
+posts = [
+    {
+        "author": "Justin Yau",
+        "date": "2/2/2019",
+        "content": "RIT CS Student",
+        "title": "About Me"
+    },
+    {
+        "author": "Bob",
+        "date": "2/2/2019",
+        "content": "RIT Game Dev",
+        "title": "Some dude"
+    }
+]
 
 
 # Use set FLASK_APP=python_file_name.py to start flask
 @app.route("/")
 @app.route("/home")
 def home():
-    return "Hello World!"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about-me")
@@ -22,3 +37,4 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
