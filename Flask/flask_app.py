112c313f153b56f6c3a7745a85d2a0aad8525446
Justin Yau <justiceman8998@gmail.com>
2019-02-02 15:58:19 -0500

    Added basic database

--- Flask/flask_app.py
+++ Flask/flask_app.py
@@ -4,14 +4,45 @@ Version: 1.1
 Description: First flask app for educational and testing purposes
 Author: Justin Yau
 Source: Tutorial followed can be found at https://www.youtube.com/watch?v=QnDWIZuWYW0
-Packages: Flask
+Packages: Flask, Flask-SQlAlchemy
 """
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__, template_folder="templates")
 
 app.config['SECRET_KEY'] = 'cc567a24de5382dff2a86adcc98c3589'
+
+# 3 forward slashes tell the program to create the database within relative directory
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    # This tells the method how the user is to be printed
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 posts = [
     {
